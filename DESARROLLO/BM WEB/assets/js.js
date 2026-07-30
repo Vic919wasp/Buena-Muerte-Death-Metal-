ece4212954490cff25c3e485a2946119004e5e98
Victor Arias <134075375+Vic919wasp@users.noreply.github.com>
2026-07-29 23:42:34 -0300

    Update: 2026-07-29 23:42

--- DESARROLLO/BM WEB/assets/js.js
+++ DESARROLLO/BM WEB/assets/js.js
@@ -37,7 +37,7 @@ var FECHAS = [
     mapa: 'https://maps.google.com/maps?q=MONTEVIDEO+175+-+CONGRESO&output=embed',
     transporte: '151 - 168 - 98 -60',
     descripcion: '⛧⛧⛧Cumple Macabro ⛧⛧⛧ \nConmemorando el cumpleaños de Ramón Macabra Records!!!!!\n⚰️Líder fundador de Dislepsia⚰️\nTOCAMOS: Dislepsia. Morferus. Uno  Mata. RIGOR MORTIS. BUENA MUERTE!!!!',
-    fotos: ['assets/tour/IMG Cumple Macabro_1.jpeg']
+    fotos: ['assets/tour/IMG Cumple Macabro_3.jpeg']
   },
   {
     dia: '24',
